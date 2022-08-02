1dc04c045bc654ee1fa9e9d5bbe669e43ac5c229
AlexGMarinho <alexg.marinhoooo>
2022-08-01 22:54:29 -0300

    express

---
+++ src/server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+});
+
+app.listen(3000, () => {
+    console.log(`Servidor esta ouvindo na porta 8080`);
+});
