856514c079c747934072ecf2ec7b5701721df559
AlexGMarinho <alexg.marinhoooo>
2022-08-03 00:56:52 -0300

    add delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -66,9 +66,9 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findByIdAndRemove(req.params.id);
-        res.status(200).json(checklist);
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        res.status(500).render('pages/error', { error: 'Erro ao deletar a Listas de tarefas' });
     }
 });
 module.exports = router;
