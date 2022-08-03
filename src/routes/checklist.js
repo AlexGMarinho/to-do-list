7ac8868bb2cd8caa39004c3ae01033308c19bfbb
AlexGMarinho <alexg.marinhoooo>
2022-08-02 23:49:30 -0300

    add put

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -20,6 +20,15 @@ router.get('/new', async (req, res) => {
     }
 });
 
+router.get('/:id/edit', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/edit', { checklist: checklist });
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir a edição de Listas de tarefas' });
+    }
+});
+
 router.post('/', async (req, res) => {
     let { name } = req.body.checklist;
     let checklist = new Checklist({ name });
@@ -42,13 +51,15 @@ router.get('/:id', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-    const { name } = req.body;
+    let { name } = req.body.checklist;
+    let checklist = await Checklist.findById(req.params.id);
 
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
-        res.status(200).json(checklist);
+        await checklist.update({ name });
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error);
+        let errors = error.erros;
+        res.status(422).render('checklists/edit', { checklist: { ...checklist, errors } });
     }
 });
 
