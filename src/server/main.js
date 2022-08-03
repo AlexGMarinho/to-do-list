4df80bb0fefdc7bc692d51443afbd9cb247ce89e
AlexGMarinho <alexg.marinhoooo>
2022-08-02 21:39:05 -0300

    methodOverride

--- src/server/main.js
+++ src/server/main.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
+
 const router = require('../routes/checklist.js');
 const rootRouter = require('../routes');
+const methodOverride = require('method-override');
 
 require('../../config/database');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, '../../public')));
 
